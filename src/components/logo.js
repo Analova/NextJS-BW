ea8eb9afcc1d7333788cbb4a51fb97f8f3a213cb
Ana Nikolova <ani.nik19@gmail.com>
2021-07-19 14:34:56 +0200

    the header and the drawer

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,9 +1,22 @@
 /** @jsx jsx */
 import { jsx, Image } from 'theme-ui';
 import { Link } from 'components/link';
+import { FaExternalLinkSquareAlt } from 'react-icons/fa';
 
 export default function Logo({ src, ...rest }) {
   return (
-    <h1>Logo</h1>
+    <Link
+      path="/"
+      sx={{
+        variant: "links.logo",
+        display: "flex",
+        cursor: "pointer",
+        mr: 15
+      }}
+      {...rest}>
+      <Image src={src} alt="landing page logo">
+
+      </Image>
+    </Link>
   );
 }
