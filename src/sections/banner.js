49cd28ff91add918b6d4011f4aec7a4f094e7b98
Ana Nikolova <ani.nik19@gmail.com>
2021-07-21 13:34:26 +0200

    banner done

--- src/sections/banner.js
+++ src/sections/banner.js
@@ -7,7 +7,24 @@ import ShapeRight from 'assets/shape-right.png';
 
 export default function Banner() {
   return (
-    <h1>Banner</h1>
+    <section sx={styles.banner} id="home">
+      <Container sx={styles.banner.container}>
+        <Box sx={styles.banner.contentBox}>
+          <Heading as="h1" variant="heroPrimary">
+            Top quiality gitital products to explore
+          </Heading>
+          <Text as="p" variant="heroSecondary">
+            Check out website to find our products and deals. If you need a website or
+            a web application that is the place to go!
+          </Text>
+          <Button variant="primary">Explore</Button>
+        </Box>
+        <Box sx={styles.banner.imageBox}>
+          <Image src={BannerImg} alt="banner" />
+
+        </Box>
+      </Container>
+    </section>
   );
 }
 
